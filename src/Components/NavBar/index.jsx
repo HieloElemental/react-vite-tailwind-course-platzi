12e533bd43b5329413f988e14b0a4002491d47ef
Hielo Elemental <hieloelemental@gmail.com>
2024-06-10 11:51:43 -0500

    perf: :art: better NabBar inner organization
    Create NavItem inside NavBar component and add renderNavItems function to do future code changes easier

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { NavLink } from "react-router-dom";
 
 const navProductCategories = [
@@ -15,44 +16,42 @@ const navRightPages = [
   { path: "/sign-in", text: "Sign In" },
 ];
 
+const NavItem = ({ path, text }) => (
+  <li>
+    <NavLink
+      className={({ isActive }) =>
+        isActive ? "underline underline-offset-4" : undefined
+      }
+      to={path}
+    >
+      {text}
+    </NavLink>
+  </li>
+);
+NavItem.propTypes = {
+  path: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
 const NavBar = () => {
-  const activeStyle = "underline underline-offset-4";
+  const renderNavItems = (navItems) => {
+    return navItems.map(({ path, text }) => {
+      return <NavItem key={path} path={path} text={text}></NavItem>;
+    });
+  };
 
   return (
     <nav className='bg-white border-gray-200 dark:bg-gray-900 dark:text-white flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
       <ul className='gap-3 flex items-center'>
         <li className='font-semibold'>
-          <NavLink
-            to='/'
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Shopi
-          </NavLink>
+          <NavLink to={"/"}>Shopi</NavLink>
         </li>
-        {navProductCategories.map((navElement) => (
-          <li key={navElement.path}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeStyle : undefined)}
-              to={navElement.path}
-            >
-              {navElement.text}
-            </NavLink>
-          </li>
-        ))}
+        {renderNavItems(navProductCategories)}
       </ul>
 
       <ul className='gap-3 flex items-center'>
         <li className='dark:text-white/60'>hieloelemental@shopi.com</li>
-        {navRightPages.map((navElement) => (
-          <li key={navElement.path}>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeStyle : undefined)}
-              to={navElement.path}
-            >
-              {navElement.text}
-            </NavLink>
-          </li>
-        ))}
+        {renderNavItems(navRightPages)}
         <li>&#128722; 0</li>
       </ul>
     </nav>
