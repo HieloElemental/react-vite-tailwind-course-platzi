3d1fc1aa12b3855866d63b8ad9dbfbceae41fd0c
Hielo Elemental <hieloelemental@gmail.com>
2024-06-10 12:17:30 -0500

    feat: :sparkles: lesson 6 - Create Layout component and integrate with App
    Created a new Layout component to provide a consistent structure and styling for the application. Integrated the Layout component into the App component to ensure uniform all pages.

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,33 +1,36 @@
-import { useRoutes, BrowserRouter } from 'react-router-dom';
+import { useRoutes, BrowserRouter } from "react-router-dom";
 
-import { Home } from '../Home';
-import { MyAccount } from '../MyAccount';
-import { MyOrder } from '../MyOrder';
-import { MyOrders } from '../MyOrders';
-import { NotFound } from '../NotFound';
-import { SignIn } from '../SignIn';
-import { NavBar } from '../../Components/NavBar';
+import { Home } from "../Home";
+import { MyAccount } from "../MyAccount";
+import { MyOrder } from "../MyOrder";
+import { MyOrders } from "../MyOrders";
+import { NotFound } from "../NotFound";
+import { SignIn } from "../SignIn";
+import { NavBar } from "../../Components/NavBar";
+import { Layout } from "../../Containers/Layout";
 
-import './App.css'
+import "./App.css";
 
 const AppRoutes = () => {
   return useRoutes([
-    { path: '/', element: <Home /> },
-    { path: '/my-account', element: <MyAccount /> },
-    { path: '/my-order', element: <MyOrder /> },
-    { path: '/my-orders', element: <MyOrders /> },
-    { path: '/sign-in', element: <SignIn /> },
-    { path: '/*', element: <NotFound /> }
-  ])
-}
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+};
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
       <NavBar />
+      <Layout>
+        <AppRoutes />
+      </Layout>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
