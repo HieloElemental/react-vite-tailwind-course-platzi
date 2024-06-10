0317d9c9526e6bbffe408cce1eae364f9b996009
Hielo Elemental <hieloelemental@gmail.com>
2024-06-09 23:20:12 -0500

    feat: :heavy_plus_sign: lesson 4 - add react-router-dom to manage all the App routes
    use of the useRoutes od react-router-dom to manage the routing of the app

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,3 +1,5 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+
 import { Home } from '../Home';
 import { MyAccount } from '../MyAccount';
 import { MyOrder } from '../MyOrder';
@@ -7,18 +9,22 @@ import { SignIn } from '../SignIn';
 
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  return useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
+  ])
+}
+
+const App = () => {
   return (
-    <>
-      <div className='bg-red-100'>
-        <Home />
-        <MyAccount />
-        <MyOrder />
-        <MyOrders />
-        <NotFound />
-        <SignIn />
-      </div>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
