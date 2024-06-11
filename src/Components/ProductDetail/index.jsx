cd76ebba5c5f0abdb3d941e8e90d7c028f6b1b89
Hielo Elemental <hieloelemental@gmail.com>
2024-06-10 21:03:30 -0500

    refactor: :lipstick: lesson 11 challenge -  Add heroicons to the UI

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,9 +1,13 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
 const ProductDetail = () => {
   return (
     <aside className='w-full h-[calc(100vh-60px)] z-10 top-[60px] right-0 rounded-l-lg max-w-md flex flex-col fixed bg-gray-900 text-white border border-gray-700'>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Details</h2>
-        <div>X</div>
+        <div>
+          <XMarkIcon className='size-6' />
+        </div>
       </div>
     </aside>
   );
