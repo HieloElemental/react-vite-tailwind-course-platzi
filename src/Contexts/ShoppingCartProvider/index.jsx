05239982d32ddadaaf0c1c4d639aac8515aa7fd6
Hielo Elemental <hieloelemental@gmail.com>
2024-06-10 22:58:06 -0500

    feat: :sparkles: lesson 15 - Implement cartProducts state in ShoppingCartProvider to save only cart products
    Add cartProducts state to ShoppingCartProvider and update the onAddHandler function to add new products to the cart
    Update the onAddHandler function in Card component to put the product as a parameter

--- src/Contexts/ShoppingCartProvider/index.jsx
+++ src/Contexts/ShoppingCartProvider/index.jsx
@@ -4,14 +4,20 @@ import { PropTypes } from "prop-types";
 const ShoppingCartContext = createContext();
 
 const ShoppingCartProvider = ({ children }) => {
+  const [cartProducts, setCartProducts] = useState([]);
   const [count, setCount] = useState(0);
 
-  const onAddHandler = () => {
-    setCount((prevCount) => prevCount + 1);
+  const onAddHandler = (newProduct) => {
+    const repeatedProduct = cartProducts.filter((p) => p.id === newProduct.id);
+    if (!repeatedProduct.length) {
+      let newCartProducts = [...cartProducts, newProduct];
+      setCartProducts(newCartProducts);
+      setCount(newCartProducts.length);
+    }
   };
 
   return (
-    <ShoppingCartContext.Provider value={{ count, onAddHandler }}>
+    <ShoppingCartContext.Provider value={{ cartProducts, count, onAddHandler }}>
       {children}
     </ShoppingCartContext.Provider>
   );
